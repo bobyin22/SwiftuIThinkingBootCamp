c8cfd3d7111616114486087f61b76278a8004294
Chou Po Yin <94157717+bobyin22@users.noreply.github.com>
2025-02-06 16:00:48 +0800

    init

---
+++ SwiftuIThinkingBootCamp/SwiftuIThinkingBootCamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftuIThinkingBootCamp
+//
+//  Created by Yin Bob on 2025/2/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftuIThinkingBootCamp/SwiftuIThinkingBootCamp/SwiftuIThinkingBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuIThinkingBootCampApp.swift
+//  SwiftuIThinkingBootCamp
+//
+//  Created by Yin Bob on 2025/2/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuIThinkingBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
