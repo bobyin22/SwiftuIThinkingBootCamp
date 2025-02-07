7b917ac73cda612066402fcccc51a7d33c563e55
bobyin <bobyin22@gmail.com>
2025-02-07 12:12:52 +0800

    [modify] EP52 use @EnvironmentObject - 透過 @EnvironmentObject 讓第一頁資料傳到第三頁 (不經過第二頁)

--- SwiftuIThinkingBootCamp/SwiftuIThinkingBootCamp/EnvironmentObjectBootcamp.swift
+++ SwiftuIThinkingBootCamp/SwiftuIThinkingBootCamp/EnvironmentObjectBootcamp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 // ObservedObject
 // StateObject
-// EnviromentObject (有點像是StateObject)
+// EnviromentObject (有點像是StateObject，但算是全域所以視情況使用)
 
 class EnvironmentViewModel: ObservableObject {
     
@@ -35,7 +35,7 @@ struct EnvironmentObjectBootcamp: View {
             List {
                 ForEach(viewModel.dataArray, id: \.self) { item in
                     NavigationLink(
-                        destination: DetailView(selectedItem: item, viewModel: viewModel),
+                        destination: DetailView(selectedItem: item), //第二頁資料在這傳的
                         label: {
                             Text(item)
                         })
@@ -43,6 +43,7 @@ struct EnvironmentObjectBootcamp: View {
             }
             .navigationTitle("iOS Devices")
         }
+        .environmentObject(viewModel) //添加environmentObject，把資料傳到最後一頁
     }
 }
 
@@ -50,7 +51,6 @@ struct EnvironmentObjectBootcamp: View {
 struct DetailView: View {
     
     let selectedItem: String
-    @ObservedObject var viewModel: EnvironmentViewModel
     
     var body: some View {
         ZStack {
@@ -59,7 +59,7 @@ struct DetailView: View {
             
             //foreground
             NavigationLink(
-                destination: FinalVIew(viewModel: viewModel),
+                destination: FinalVIew(),
                 label: {
                     Text(selectedItem)
                         .font(.headline)
@@ -76,7 +76,7 @@ struct DetailView: View {
 // MARK: VC3
 struct FinalVIew: View {
     
-    @ObservedObject var viewModel: EnvironmentViewModel
+    @EnvironmentObject var viewModel: EnvironmentViewModel //直接拿到第一頁資料
     
     var body: some View {
         ZStack{
